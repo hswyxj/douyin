cf8aeefe4451b1e6f065677caa0715d2abfeea23
hswyxj <hesw@yyft.com>
2020-09-12 11:49:45 +0800

    1、点击视频位置可播放停止视频 2、新增video参数，适配手机浏览器（并不是所有手机都兼容）

--- pages/douyin/index.vue
+++ pages/douyin/index.vue
@@ -1,30 +1,39 @@
 <template>
 	<view class="page">
 		<swiper class="swiper" :circular="circular" :vertical="true" @change="onSwiperChange">
-			<swiper-item v-for="item in videoList" :key="item.id">
-				<video class="video" :id="item.id" :ref="item.id" :src="item.src" :controls="false" :loop="true"
-				 :show-center-play-btn="false" object-fit='fill'></video>
-				<image src="/static/head.jpg" class="video-image">
-				<view class="video-love" @click="love(item)">
-					<uni-icons type="heart-filled" :color="isactive==true?'#f44336':'#ffffff'" size="55" />
-					<view class="video-num">{{item.love_num}}</view>
-				</view>
-				<view class="video-comm" @click="comm">
-					<uni-icons type="chat-filled" color="#ffffff" size="50" />
-					<view class="video-num">{{item.comm_num}}</view>
-				</view>
-				<view class="video-redo" @click="redo">
-					<uni-icons type="redo-filled" color="#ffffff" size="50" />
-					<view class="video-num">{{item.redo_num}}</view>
-				</view>
-
+			<swiper-item v-for="(item,index) in videoList" :key="item.id">
+				<video @click="videoplay(index)" class="video" :id="item.id" :ref="item.id" :src="item.src" :controls="false" 
+				 :loop="true"
+				 :show-center-play-btn="false" 
+				 preload="auto" 
+			   object-fit='fill' 
+				 x5-playsinline="" 
+			   playsinline="true"
+				 webkit-playsinline="true" 
+				 x-webkit-airplay="allow"
+				 x5-video-player-type="h5" 
+				 x5-video-player-fullscreen=""
+				 x5-video-orientation="portraint">
+				 <cover-image :id="item.id+'img'" :src="item.img" class="video-image"></cover-image>
+				 <cover-view class="video-love" @click="love(item)">
+				 	<uni-icons type="heart-filled" :color="isactive==true?'#f44336':'#ffffff'" size="55" />
+				 	<view class="video-num">{{item.love_num}}</view>
+				 </cover-view>
+				 <cover-view class="video-comm" @click="comm">
+				 	<uni-icons type="chat-filled" color="#ffffff" size="50" />
+				 	<view class="video-num">{{item.comm_num}}</view>
+				 </cover-view>
+				 <cover-view class="video-redo" @click="redo">
+				 	<uni-icons type="redo-filled" color="#ffffff" size="50" />
+				 	<view class="video-num">{{item.redo_num}}</view>
+				 </cover-view>
+				</video>	
 			</swiper-item>
 		</swiper>
 	</view>
 </template>
 <script>
-	const videoData = [
-		{
+	const videoData = [{
 			src: 'https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/hellouniapp/hello-nvue-swiper-vertical-01.mp4'
 		},
 		{
@@ -47,28 +56,28 @@
 	export default {
 		data() {
 			return {
-				isactive:false,
+				isactive: false,
 				circular: true,
 				videoList: [{
 						id: "video0",
-						src: "",
-						img: "",
+						src: "https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/hellouniapp/hello-nvue-swiper-vertical-01.mp4",
+						img: "/static/head.jpg",
 						love_num: 101,
 						comm_num: 1431,
 						redo_num: 1001,
 					},
 					{
 						id: "video1",
-						src: "",
-						img: "",
+						src: "https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/hellouniapp/hello-nvue-swiper-vertical-01.mp4",
+						img: "/static/head.jpg",
 						love_num: 1100,
 						comm_num: 1132,
 						redo_num: 1002,
 					},
 					{
 						id: "video2",
-						src: "",
-						img: "",
+						src: "https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/hellouniapp/hello-nvue-swiper-vertical-01.mp4",
+						img: "/static/head.jpg",
 						love_num: 3100,
 						comm_num: 1233,
 						redo_num: 1003,
@@ -86,10 +95,18 @@
 			init() {
 				this._videoIndex = 0;
 				this._videoContextList = [];
+				console.log(this)
 				for (var i = 0; i < this.videoList.length; i++) {
 					this._videoContextList.push(uni.createVideoContext('video' + i, this));
 				}
 				this._videoDataIndex = 0;
+			},
+			videoplay(index) {
+				const id = this._videoContextList[index].id
+				const video = document.getElementById(`${id}`).firstChild.firstChild
+				video.paused ?
+					this._videoContextList[index].play() :
+					this._videoContextList[index].pause()
 			},
 			getData(e) {
 				this.videoDataList = videoData;
@@ -159,21 +176,27 @@
 			updateVideo(isNext) {
 				this.$set(this.videoList[this._videoIndex], 'src', this.videoDataList[this._videoDataIndex].src);
 				this.$set(this.videoList[this.getNextIndex(isNext)], 'src', this.videoDataList[this.getNextDataIndex(isNext)].src);
+				
 				setTimeout(() => {
+					this._videoContextList[this._videoIndex].hideStatusBar();
 					this._videoContextList[this._videoIndex].play();
 				}, 200);
 				console.log("v:" + this._videoIndex + " d:" + this._videoDataIndex + "; next v:" + this.getNextIndex(
 					isNext) + " next d:" + this.getNextDataIndex(isNext));
 			},
-			love(item){
+			love(item) {
 				item.love_num += 1
 				this.isactive = !this.isactive
 			},
-			comm(){
-				uni.showToast({title: '点击评论'});
+			comm() {
+				uni.showToast({
+					title: '点击评论'
+				});
 			},
-			redo(){
-				uni.showToast({title: '点击分享'});
+			redo() {
+				uni.showToast({
+					title: '点击分享'
+				});
 			}
 		}
 	}
@@ -205,11 +228,16 @@
 	}
 
 	.video {
+		position: relative;
 		flex: 1;
 		/* #ifndef APP-PLUS */
 		width: 100%;
 		/* #endif */
 		height: 100vh;
+		z-index: 0;
+	}
+	cover-view,cover-image {
+	  display: inline-block;
 	}
 
 	.video-image {
@@ -220,35 +248,40 @@
 		width: 60px;
 		border-radius: 50%;
 		border: 3px solid #fff;
+		z-index: 100;
 	}
 
 	.video-love {
 		position: fixed;
-		height: 60px;
-		bottom: 52vh;
+		height: 80px;
+		bottom: 46vh;
 		right: 15px;
+		z-index: 100;
 	}
 
 	.video-num {
 		text-align: center;
 		position: relative;
-		bottom: 26px;
+		bottom: 10px;
 		font-size: 14px;
 		font-weight: bold;
 		color: #FFFFFF;
+		z-index: 100;
 	}
 
 	.video-comm {
 		position: fixed;
-		height: 60px;
-		bottom: 39vh;
+		height: 80px;
+		bottom: 32vh;
 		right: 15px;
+		z-index: 100;
 	}
 
 	.video-redo {
 		position: fixed;
-		height: 60px;
-		bottom: 28vh;
+		height: 80px;
+		bottom: 19vh;
 		right: 15px;
+		z-index: 100;
 	}
 </style>
